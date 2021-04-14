98d465cff059ba864e9d615e2b8270da52a20790
zstapel <70587449+zstapel@users.noreply.github.com>
2021-04-14 16:13:04 -0500

    WOOOO done!

--- main.js
+++ main.js
@@ -1,67 +1,606 @@
 console.log("Hello Cereal");
 
-var Airtable = require('airtable');
+
+$( function() {
+    $( "#letters h3" ).draggable();
+  } );
+
+function resetLetters () {
+	let a = document.querySelector(".a")
+	a.style.top = "55px";
+	a.style.left = "161px";
+	let b = document.querySelector(".b")
+	b.style.top = "65px";
+	a.style.left = "170px";
+	let c = document.querySelector(".c")
+	c.style.top = "120px";
+	c.style.left = "210px";
+	let d = document.querySelector(".d")
+	d.style.top = "90px";
+	d.style.left = "90px";
+	let e = document.querySelector(".e")
+	e.style.top = "67px";
+	e.style.left = "367px";
+	let f = document.querySelector(".f")
+	f.style.top = "20px";
+	f.style.left = "430px";
+	let g = document.querySelector(".g")
+	g.style.top = "40px";
+	g.style.left = "250px";
+	let h = document.querySelector(".h")
+	h.style.top = "45px";
+	h.style.left = "270px";
+	let i = document.querySelector(".i")
+	i.style.top = "34px";
+	i.style.left = "40px";
+	let j  = document.querySelector(".j")
+	j.style.top = "5px";
+	j.style.left = "76px";
+	let k = document.querySelector(".k")
+	k.style.top = "27px";
+	k.style.left = "167px";
+	let l = document.querySelector(".l")
+	l.style.top = "46px";
+	l.style.left = "78px";
+	let m = document.querySelector(".m")
+	m.style.top = "60px";
+	m.style.left = "249px";
+	let n = document.querySelector(".n")
+	n.style.top = "34px";
+	n.style.left = "345px";
+	let o = document.querySelector(".o")
+	o.style.top = "3px";
+	o.style.left = "275px";
+	let p = document.querySelector(".p")
+	p.style.top = "14px";
+	p.style.left = "239px";
+	let q = document.querySelector(".q")
+	q.style.top = "100px";
+	q.style.left = "354px";
+	let r = document.querySelector(".r")
+	r.style.top = "90px";
+	r.style.left = "430px";
+	let s = document.querySelector(".s")
+	s.style.top = "50px";
+	s.style.left = "290px";
+	let t = document.querySelector(".t")
+	t.style.top = "30px";
+	t.style.left = "390px";
+	let u = document.querySelector(".u")
+	u.style.top = "26px";
+	u.style.left = "135px";
+	let v = document.querySelector(".v")
+	v.style.top = "45px";
+	v.style.left = "376px";
+	let w = document.querySelector(".w")
+	w.style.top = "12px";
+	w.style.left = "301px";
+	let x = document.querySelector(".x")
+	x.style.top = "7px";
+	x.style.left = "396px";
+	let y = document.querySelector(".y")
+	y.style.top = "12px";
+	y.style.left = "440px";
+	let z = document.querySelector(".z")
+	z.style.top = "100px";
+	z.style.left = "130px";
+	let a1 = document.querySelector(".a1")
+	a1.style.top = "15px";
+	a1.style.left = "123px";
+	let a2 = document.querySelector(".a2")
+	a2.style.top = "3px";
+	a2.style.left = "151px";
+	let a3 = document.querySelector(".a3")
+	a3.style.top = "4px";
+	a3.style.left = "361px";
+	let a4 = document.querySelector(".a4")
+	a4.style.top = "105px";
+	a4.style.left = "276px";
+	let a5 = document.querySelector(".a5")
+	a5.style.top = "75px";
+	a5.style.left = "311px";
+	let e1 = document.querySelector(".e1")
+	e1.style.top = "67px";
+	e1.style.left = "200px";
+	let e2 = document.querySelector(".e2")
+	e2.style.top = "7px";
+	e2.style.left = "267px";
+	let e3 = document.querySelector(".e3")
+	e3.style.top = "87px";
+	e3.style.left = "49px";
+	let e4 = document.querySelector(".e4")
+	e4.style.top = "47px";
+	e4.style.left = "300px";
+	let e5 = document.querySelector(".e5")
+	e5.style.top = "0px";
+	e5.style.left = "197px";
+	let i1 = document.querySelector(".i1")
+	i1.style.top = "44px";
+	i1.style.left = "210px";
+	let i2 = document.querySelector(".i2")
+	i2.style.top = "84px";
+	i2.style.left = "240px";
+	let i3 = document.querySelector(".i3")
+	i3.style.top = "64px";
+	i3.style.left = "400px";
+	let i4 = document.querySelector(".i4")
+	i4.style.top = "89px";
+	i4.style.left = "180px";
+	let i5 = document.querySelector(".i5")
+	i5.style.top = "64px";
+	i5.style.left = "120px";
+	let o1 = document.querySelector(".o1")
+	o1.style.top = "0px";
+	o1.style.left = "375px";
+	let o2 = document.querySelector(".o2")
+	o2.style.top = "30px";
+	o2.style.left = "320px";
+	let o3 = document.querySelector(".o3")
+	o3.style.top = "98px";
+	o3.style.left = "315px";
+	let o4 = document.querySelector(".o4")
+	o4.style.top = "50px";
+	o4.style.left = "475px";
+	let o5 = document.querySelector(".o5")
+	o5.style.top = "78px";
+	o5.style.left = "275px";
+	let u1 = document.querySelector(".u1")
+	u1.style.top = "66px";
+	u1.style.left = "435px";
+	let u2 = document.querySelector(".u2")
+	u2.style.top = "96px";
+	u2.style.left = "155px";
+	let u3 = document.querySelector(".u3")
+	u3.style.top = "6px";
+	u3.style.left = "335px";
+	let u4 = document.querySelector(".u4")
+	u4.style.top = "76px";
+	u4.style.left = "335px";
+	let u5 = document.querySelector(".u5")
+	u5.style.top = "96px";
+	u5.style.left = "210px";
+	let b1 = document.querySelector(".b1")
+	b1.style.top = "25px";
+	b1.style.left = "70px";
+	let b2 = document.querySelector(".b2")
+	b2.style.top = "95px";
+	b2.style.left = "460px";
+	let b3 = document.querySelector(".b3")
+	b3.style.top = "25px";
+	b3.style.left = "210px";
+	let c1 = document.querySelector(".c1")
+	c1.style.top = "50px";
+	c1.style.left = "410px";
+	let c2 = document.querySelector(".c2")
+	c2.style.top = "50px";
+	c2.style.left = "330px";
+	let c3 = document.querySelector(".c3")
+	c3.style.top = "60px";
+	c3.style.left = "50px";
+	let d1 = document.querySelector(".d1")
+	d1.style.top = "70px";
+	d1.style.left = "290px";
+	let d2 = document.querySelector(".d2")
+	d2.style.top = "60px";
+	d2.style.left = "225px";
+	let d3 = document.querySelector(".d3")
+	d3.style.top = "34px";
+	d3.style.left = "100px";
+	let f1 = document.querySelector(".f1")
+	f1.style.top = "70px";
+	f1.style.left = "130px";
+	let f2 = document.querySelector(".f2")
+	f2.style.top = "110px";
+	f2.style.left = "390px";
+	let f3 = document.querySelector(".f3")
+	f3.style.top = "5px";
+	f3.style.left = "130px";
+	let g1 = document.querySelector(".g1")
+	g1.style.top = "0px";
+	g1.style.left = "245px";
+	let g2 = document.querySelector(".g2")
+	g2.style.top = "90px";
+	g2.style.left = "390px";
+	let g3 = document.querySelector(".g3")
+	g3.style.top = "40px";
+	g3.style.left = "450px";
+	let h1 = document.querySelector(".h1")
+	h1.style.top = "35px";
+	h1.style.left = "290px";
+	let h2 = document.querySelector(".h2")
+	h2.style.top = "75px";
+	h2.style.left = "70px";
+	let h3 = document.querySelector(".h3")
+	h3.style.top = "25px";
+	h3.style.left = "370px";
+	let j1 = document.querySelector(".j1")
+	j1.style.top = "15px";
+	j1.style.left = "106px";
+	let j2 = document.querySelector(".j2")
+	j2.style.top = "25px";
+	j2.style.left = "186px";
+	let j3 = document.querySelector(".j3")
+	j3.style.top = "5px";
+	j3.style.left = "206px";
+	let k1 = document.querySelector(".k1")
+	k1.style.top = "87px";
+	k1.style.left = "407px";
+	let k2 = document.querySelector(".k2")
+	k2.style.top = "64px";
+	k2.style.left = "143px";
+	let k3 = document.querySelector(".k3")
+	k3.style.top = "107px";
+	k3.style.left = "247px";
+	let l1 = document.querySelector(".l1")
+	l1.style.top = "66px";
+	l1.style.left = "378px";
+	let l2 = document.querySelector(".l2")
+	l2.style.top = "46px";
+	l2.style.left = "178px";
+	let l3 = document.querySelector(".l3")
+	l3.style.top = "36px";
+	l3.style.left = "408px";
+	let l4 = document.querySelector(".l4")
+	l4.style.top = "26px";
+	l4.style.left = "278px";
+	let l5 = document.querySelector(".l5")
+	l5.style.top = "96px";
+	l5.style.left = "108px";
+	let m1 = document.querySelector(".m1")
+	m1.style.top = "70px";
+	m1.style.left = "99px";
+	let m2 = document.querySelector(".m2")
+	m2.style.top = "110px";
+	m2.style.left = "299px";
+	let m3 = document.querySelector(".m3")
+	m3.style.top = "110px";
+	m3.style.left = "149px";
+	let m4 = document.querySelector(".m4")
+	m4.style.top = "100px";
+	m4.style.left = "179px";
+	let m5 = document.querySelector(".m5")
+	m5.style.top = "80px";
+	m5.style.left = "219px";
+	let n1 = document.querySelector(".n1")
+	n1.style.top = "79px";
+	n1.style.left = "375px";
+	let n2 = document.querySelector(".n2")
+	n2.style.top = "4px";
+	n2.style.left = "95px";
+	let n3 = document.querySelector(".n3")
+	n3.style.top = "0px";
+	n3.style.left = "165px";
+	let p1 = document.querySelector(".p1")
+	p1.style.top = "35px";
+	p1.style.left = "230px";
+	let p2 = document.querySelector(".p2")
+	p2.style.top = "0px";
+	p2.style.left = "227px";
+	let p3 = document.querySelector(".p3")
+	p3.style.top = "-20px";
+	p3.style.left = "259px";
+	let p4 = document.querySelector(".p4")
+	p4.style.top = "114px";
+	p4.style.left = "369px";
+	let p5 = document.querySelector(".p5")
+	p5.style.top = "94px";
+	p5.style.left = "263px";
+	let q1 = document.querySelector(".q1")
+	q1.style.top = "49px";
+	q1.style.left = "124px";
+	let q2 = document.querySelector(".q2")
+	q2.style.top = "56px";
+	q2.style.left = "354px";
+	let r1 = document.querySelector(".r1")
+	r1.style.top = "-18px";
+	r1.style.left = "360px";
+	let r2 = document.querySelector(".r2")
+	r2.style.top = "110px";
+	r2.style.left = "330px";
+	let r3 = document.querySelector(".r3")
+	r3.style.top = "60px";
+	r3.style.left = "320px";
+	let s1 = document.querySelector(".s1")
+	s1.style.top = "-10px";
+	s1.style.left = "390px";
+	let s2 = document.querySelector(".s2")
+	s2.style.top = "94px";
+	s2.style.left = "290px";
+	let s3 = document.querySelector(".s3")
+	s3.style.top = "50px";
+	s3.style.left = "100px";
+	let s4 = document.querySelector(".s4")
+	s4.style.top = "-4px";
+	s4.style.left = "300px";
+	let s5 = document.querySelector(".s5")
+	s5.style.top = "5px";
+	s5.style.left = "64px";
+	let t1 = document.querySelector(".t1")
+	t1.style.top = "-7px";
+	t1.style.left = "120px";
+	let t2 = document.querySelector(".t2")
+	t2.style.top = "70px";
+	t2.style.left = "190px";
+	let t3 = document.querySelector(".t3")
+	t3.style.top = "80px";
+	t3.style.left = "145px";
+	let t4 = document.querySelector(".t4")
+	t4.style.top = "40px";
+	t4.style.left = "150px";
+	let t5 = document.querySelector(".t5")
+	t5.style.top = "86px";
+	t5.style.left = "360px";
+	let v1 = document.querySelector(".v1")
+	v1.style.top = "-15px";
+	v1.style.left = "336px";
+	let v2 = document.querySelector(".v2")
+	v2.style.top = "43px";
+	v2.style.left = "190px";
+	let w1 = document.querySelector(".w1")
+	w1.style.top = "-5px";
+	w1.style.left = "180px";
+	let w2 = document.querySelector(".w2")
+	w2.style.top = "-15px";
+	w2.style.left = "140px";
+	let w3 = document.querySelector(".w3")
+	w3.style.top = "-10px";
+	w3.style.left = "211px";
+	let x1 = document.querySelector(".x1")
+	x1.style.top = "98px";
+	x1.style.left = "374px";
+	let x2 = document.querySelector(".x2")
+	x2.style.top = "103px";
+	x2.style.left = "230px";
+	let y1 = document.querySelector(".y1")
+	y1.style.top = "42px";
+	y1.style.left = "430px";
+	let y2 = document.querySelector(".y2")
+	y2.style.top = "-7px";
+	y2.style.left = "320px";
+	let y3 = document.querySelector(".y3")
+	y3.style.top = "82px";
+	y3.style.left = "250px";
+	let y4 = document.querySelector(".y4")
+	y4.style.top = "-5px";
+	y4.style.left = "415px";
+	let y5 = document.querySelector(".y5")
+	y5.style.top = "-16px";
+	y5.style.left = "285px";
+	let z1 = document.querySelector(".z1")
+	z1.style.top = "70px";
+	z1.style.left = "420px";
+	let z2 = document.querySelector(".z2")
+	z2.style.top = "20px";
+	z2.style.left = "350px";
+	let z3 = document.querySelector(".z3")
+	z3.style.top = "95px";
+	z3.style.left = "335px";
+};
+
+
+
+var Airtable = require("airtable");
 var base = new Airtable({apiKey: 'key3CZWKwGJ23LuAr'}).base('app0e7F32drzROp2H');
 
-base("Table 1").select({}).eachPage(gotPageOfTable1, gotAllTable1);
-const Table1 = [];
-function gotPageOfTable1(records, fetchNextPage) {
-	console.log("gotPageOfTable1()");
-	Table1.push(...records);
+base("cereal_collection").select({
+	view: "nameTags"
+}).eachPage(gotPageOfCereal_collection, gotAllCereal_collection);
+const cereals = [];
+function gotPageOfCereal_collection(records, fetchNextPage) {
+	console.log("gotPageOfCereal_collection()");
+	cereals.push(...records);
 	fetchNextPage();
 }
-function gotAllTable1(err) {
-	console.log("gotAllTable1()");
+function gotAllCereal_collection(err) {
+	console.log("gotAllCereal_collection()");
 
 	if (err) {
-		console.log("error loading Table1");
+		console.log("error loading cereal_collection");
 		console.error(err);
 		return;
 	}
 
-	showTable1();
+	showCereal_collection();
 }
-function showTable1() {
-	console.log("showTable1()");
+function showCereal_collection() {
+	console.log("showCereal_collection()");
 
-	const Cereals = [];
-	const CerealsContainer = document.querySelector
-	("#container");
+	// const Cereals = [];
+	const cerealsContainer = document.querySelector("#container");
 
-	Cereals.forEach((cereal) => {
+	 cereals.forEach((cereal) => {
 		console.log("\nCereals data fields:")
+		console.log(cereal.fields)
 
-		const CerealsCover_image = document.createElement("img");
+		const cerealContainer = document.createElement("div");
+		cerealContainer.classList.add("cerealContainer");
 
-		CerealsCover_image.src = Cereals.fields.cover_image[0].url;
+		const cerealsCover_image = document.createElement("img");
+		cerealsCover_image.src = cereal.fields.cover_image[0].url;
+		console.log(cereal.fields.cover_image[0].url);
+		cerealContainer.appendChild(cerealsCover_image);
 
-		CerealsContainer.appendChild(CerealsCover_image);
+		// const cerealsTitle = document.createElement("h1");
+		// cerealsTitle.innerHTML = cereal.fields.title;
+		// cerealContainer.appendChild(cerealsTitle);
 
-		const CerealsTitle = document.createElement("h1")
+		// const cerealsDescription  = document.createElement("p");
+		// cerealsDescription.innerHTML = cereal.fields.description;
+		// cerealContainer.appendChild(cerealsDescription);
 
-		CerealsTitle.innerHTML = Cereals.fields.Title;
+		// const cerealsBrand  = document.createElement("p");
+		// cerealsBrand.innerHTML = cereal.fields.brand;
+		// cerealContainer.appendChild(cerealsBrand);
 
-		CerealsContainer.appendChild(CerealsTitle);
+		// const cerealsTags  = document.createElement("p");
+		// cerealsTags.innerHTML = cereal.fields.tags;
+		// cerealContainer.appendChild(cerealsTags);
 
-		const CerealDescription  = document.createElement("p");
+		let cerealTagsList = cereal.fields.tags;
 
-		CerealsDescription.innerHTML = Cereals.fields.Description;
 
-		CerealsDescription.appendChild(CerealsDescription);
+		cerealTagsList.forEach(function(tag){
+			cerealContainer.classList.add(tag)
 
-		const CerealsBrand  = document.createElement("p");
+		
+			
+		})
+		let cerealBrandList = cereal.fields.brand;
 
-		CerealsBrand.innerHTML = Cereals.fields.brand;
+		cerealBrandList.forEach(function(brand){
+			cerealContainer.classList.add(brand)
+		})
 
-		CerealsBrand.appendChild(CerealsBrand);
+		var filterHealthy = document.getElementById('healthy');
+     	filterHealthy.addEventListener("click", function(){
+        if (cerealContainer.classList.contains("healthy")) {
+          cerealContainer.style.display = "block";
+        } else {
+          cerealContainer.style.display = "none";
+        }
+    })
 
-		const CerealsTags  = document.createElement("p");
+        var filterHoney = document.getElementById('honey');
+     	filterHoney.addEventListener("click", function(){
+        if (cerealContainer.classList.contains("honey")) {
+          cerealContainer.style.display = "block";
+        } else {
+          cerealContainer.style.display = "none";
+        }
 
-		CerealsTags.innerHTML = Cereals.fields.tags;
+      })
 
-		CerealsTags.appendChild(CerealsTags);
+     	var filterChocolate = document.getElementById('chocolate');
+     	filterChocolate.addEventListener("click", function(){
+        if (cerealContainer.classList.contains("chocolate")) {
+          cerealContainer.style.display = "block";
+        } else {
+          cerealContainer.style.display = "none";
+        }
+
+      })
+
+     	var filterSpecial = document.getElementById('special');
+     	filterSpecial.addEventListener("click", function(){
+        if (cerealContainer.classList.contains("special")) {
+          cerealContainer.style.display = "block";
+        } else {
+          cerealContainer.style.display = "none";
+        }
+
+      })
+
+     	var filterClassic = document.getElementById('classic');
+     	filterClassic.addEventListener("click", function(){
+        if (cerealContainer.classList.contains("classic")) {
+          cerealContainer.style.display = "block";
+        } else {
+          cerealContainer.style.display = "none";
+        }
+
+      })
+
+     	var filterFruity = document.getElementById('fruity');
+     	filterFruity.addEventListener("click", function(){
+        if (cerealContainer.classList.contains("fruity")) {
+          cerealContainer.style.display = "block";
+        } else {
+          cerealContainer.style.display = "none";
+        }
+
+      })
+
+     	var filterPeanutButter = document.getElementById('peanut_butter');
+     	filterPeanutButter.addEventListener("click", function(){
+        if (cerealContainer.classList.contains("peanut_butter")) {
+          cerealContainer.style.display = "block";
+        } else {
+          cerealContainer.style.display = "none";
+        }
+
+      })
+
+     	var filterCinnamon = document.getElementById('cinnamon');
+     	filterCinnamon.addEventListener("click", function(){
+        if (cerealContainer.classList.contains("cinnamon")) {
+          cerealContainer.style.display = "block";
+        } else {
+          cerealContainer.style.display = "none";
+        }
+
+      })
+
+     	var filterNut = document.getElementById('nut');
+     	filterNut.addEventListener("click", function(){
+        if (cerealContainer.classList.contains("nut")) {
+          cerealContainer.style.display = "block";
+        } else {
+          cerealContainer.style.display = "none";
+        }
+
+      })
+
+     	var filterGeneralMills = document.getElementById('general_mills');
+     	filterGeneralMills.addEventListener("click", function(){
+        if (cerealContainer.classList.contains("general_mills")) {
+          cerealContainer.style.display = "block";
+        } else {
+          cerealContainer.style.display = "none";
+        }
+
+      })
+
+     	var filterKellogs = document.getElementById('kellogs');
+     	filterKellogs.addEventListener("click", function(){
+        if (cerealContainer.classList.contains("kellogs")) {
+          cerealContainer.style.display = "block";
+        } else {
+          cerealContainer.style.display = "none";
+        }
+
+      })
+
+     	var filterQuaker = document.getElementById('quaker');
+     	filterQuaker.addEventListener("click", function(){
+        if (cerealContainer.classList.contains("quaker")) {
+          cerealContainer.style.display = "block";
+        } else {
+          cerealContainer.style.display = "none";
+        }
+
+      })
+
+		cerealsContainer.appendChild(cerealContainer);
+
+		cerealContainer.addEventListener("click", function(){
+			console.log("working?")
+			var cerealsDescription = document.querySelector('#slogan')
+			cerealsDescription.innerHTML = cereal.fields.description;
+			document.getElementById('letters').style.display = "block";
+
+		})
+
+		// function resetSlogan(){
+		// 	var cerealsDescription = document.querySelector('#slogan')
+		// 	cerealsDescription.innerHTML = cereal.fields.description;
+		// 	cerealContainer.style.display ="none";
+		// };
+
+		var reset = document.getElementById("reset");
+		reset.addEventListener("click", function(){
+		resetLetters();
+		cerealContainer.style.display = "block";
+		// cerealsDescription.style.display = "none";
+		var slogan = document.getElementById('slogan')
+		slogan.innerHTML = " ";
+
+		});
+
 	});
+
+
 }
+
 
 
 // function consoleLogTable1() {
